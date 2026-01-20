867379ca687c1549b5f3b8543f2f3d3d97a943fb
Rohan Landage <138840133+rohanlndage@users.noreply.github.com>
2026-01-20 21:03:59 +0530

    20 jan 2026
    averrage-  for findng average of number 
     classINJava --- class and boj example 
    Factorial ---factorial example 
    user_value---getting input from user

---
+++ User_value.java
@@ -0,0 +1,10 @@
+import java.util.*; //-- package for scanner class package
+public class User_value {
+    
+    public static void main (String args[]){
+        Scanner sc =new Scanner (System.in);  // creating scanner object
+        System.out.println("Enter the value of a:");
+        int a=sc.nextInt();
+        System.out.println( "this is user value " + a);
+    }
+}
---
+++ classINJava.java
@@ -0,0 +1,16 @@
+public class classINJava {
+    void dispaly(){
+        System.out.println("this is class in java");
+    int a=10;
+    String name="rohan landage ";
+    System.out.println("my name is "+ name);
+    System.out.println("THis is the roll no "+a);
+    }
+    public static void main(String args[]){
+        classINJava j=new classINJava();
+        // System.out.println("value of a is : " + j.a);
+        // System.out.println("value of name is : " + j.name);
+        j.dispaly();
+
+    }
+}
